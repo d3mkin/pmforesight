f652801622086204b27ece0c04e76b80bcc106bc
Demian Brovkin <bro.d3mk@gmail.com>
2021-11-01 00:50:09 +0300

    ATEST-110 fixed Admin tests

--- src/test/java/pages/administration/PagesPage.java
+++ src/test/java/pages/administration/PagesPage.java
@@ -11,7 +11,7 @@ public class PagesPage extends AbstractAdminPage {
     public PagesPage() {
         super(
                 Configuration.baseUrl + URLS.PAGES,
-                "Мета-страницы",
+                "MetaPage",
                 $(".f-page__grid-name"),
                 $("#mainBodyContainer")
         );
